c13f55f4ecc3b549682eb91695bbd9e264d64c3c
Ted Yadeta <54333082+TedYadeta@users.noreply.github.com>
2020-07-20 09:26:12 -0700

    Added bash, batch, css, go, php and ruby
    Added the respective code for Bash, Batch Script, Go, PHP and Ruby

--- script.js
+++ script.js
@@ -8,10 +8,19 @@
 		var choiceInLowerCase = choice.toLowerCase();
 		
 		if(choiceInLowerCase == 'basic') {
-			p.textContent = "10 PRINT 'Hello, World!'\n20 END";
+			p.textContent = "10 PRINT \"Hello, World!\"\n20 END";
+			
+		} else if(choiceInLowerCase == 'bash') {
+			p.textContent = "echo \"Hello, World!\"";
+			
+		} else if(choiceInLowerCase == 'batch') {
+			p.textContent = "ECHO Hello, World!\nPAUSE";
 			
 		} else if(choiceInLowerCase == 'brainfuck') {
 			p.textContent = '++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.';
+		
+		} else if(choiceInLowerCase == 'css') {
+			p.textContent = 'I\'m terribly sorry, but CSS is a style language. I don\'t really have anything to show you...';
 		
 		} else if(choiceInLowerCase == 'c') {
 			p.textContent = '#include<stdio.h>\nint main(void) {\n\tprintf("Hello, World!");\n\treturn 0;\n}';
@@ -20,7 +29,10 @@
 			p.textContent = '#include <iostream>\nint main()\n{\n\tstd::cout << "Hello, World!" << std::endl;\n\treturn 0;\n}';
 		
 		} else if(choiceInLowerCase == 'c#') {
-			p.textContent = "Console.WriteLine('Hello, World!')";
+			p.textContent = "Console.WriteLine(\"Hello, World!\")";
+			
+		} else if(choiceInLowerCase == 'go') {
+			p.textContent = "package main\n\nimport \"fmt\"\n\nfunc main() {\n\tfmt.Println(\"Hello, World!\")\n}";
 		
 		} else if(choiceInLowerCase == 'html') {
 			p.textContent = "<DOCTYPE html>\n<head>\n\t<title></title>\n\t<body>\n\t\t<p>Hello, World!</p>\n\t</body>\n</html>";
@@ -29,10 +41,17 @@
 			p.textContent = "public class HelloWorld {\n\tpublic static void main(String[] args){\n\t\tSystem.out.print('Hello, World!');\n\t}\n}";
 			
 		} else if(choiceInLowerCase == 'javascript') {
-			p.textContent = "console.log('Hello, World!');";
+			p.textContent = "console.log('Hello, World!)";
+			
+		} else if(choiceInLowerCase == 'php') {
+			p.textContent = "<?php\n\tprint 'Hello, World!';\n?>";
 		
 		} else if(choiceInLowerCase == 'python') {
 			p.textContent = "print('Hello, World!')";
+			
+		} else if(choiceInLowerCase == 'ruby') {
+			p.textContent = "puts 'Hello, World!'";
+		
 		} else {
 			p.textContent = "Sorry, I don't have anything for that...";
 		}
