71c13c821b15f89045ecb3736eb7b5548855c50c
Ted Yadeta <54333082+TedYadeta@users.noreply.github.com>
2020-07-21 08:43:52 -0700

    Small syntax change

--- script.js
+++ script.js
@@ -41,7 +41,7 @@
 			p.textContent = "public class HelloWorld {\n\tpublic static void main(String[] args){\n\t\tSystem.out.print('Hello, World!');\n\t}\n}";
 			
 		} else if(choiceInLowerCase == 'javascript') {
-			p.textContent = "console.log('Hello, World!)";
+			p.textContent = "console.log('Hello, World!')";
 			
 		} else if(choiceInLowerCase == 'php') {
 			p.textContent = "<?php\n\tprint 'Hello, World!';\n?>";
